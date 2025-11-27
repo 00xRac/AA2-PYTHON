b797f5688555d1de989763a50a93ada1e0a13638
Oscar Ferre <oscar.ferre@estudiant.enti.cat>
2025-11-27 12:05:58 +0100

    Utils file
    FIle with util functions that will be used ny the main program

---
+++ utils.py
@@ -0,0 +1,11 @@
+import re
+from enigma_constants import ALPHABET
+
+def preprocess_message(text):
+    text = text.upper()
+    text = text.replace('À', 'A').replace('È', 'E').replace('É', 'E')
+    text = text.replace('Í', 'I').replace('Ò', 'O').replace('Ó', 'O')
+    text = text.replace('Ú', 'U').replace('Ü', 'U')
+    return re.sub(r'[^A-Z]', '', text)
+
+
