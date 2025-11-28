788f713c7bb388eaba7e98149c0a7a6de2de24aa
Oscar Ferre <oscar.ferre@estudiant.enti.cat>
2025-11-28 11:05:12 +0100

    Create rotor_manager.py
    Added object class rotor to manage rotors

---
+++ rotor_manager.py
@@ -0,0 +1,22 @@
+from enigma_constants import ALPHABET
+
+class Rotor:
+    def __init__(self, index, wiring, notch="Z"):
+        self.index = index
+        self.wiring = wiring
+        self.notch = notch.upper()
+        self.position = 0
+        self.inverse = get_inverse_wiring(wiring)
+
+    def rotate(self):
+        self.position = (self.position + 1) % 26
+
+    def forward(self, ch):
+        i = (ALPHABET.index(ch) + self.position) % 26
+        c = self.wiring[i]
+        return ALPHABET[(ALPHABET.index(c) - self.position) % 26]
+
+    def backward(self, ch):
+        i = (ALPHABET.index(ch) + self.position) % 26
+        c = self.inverse[i]
+        return ALPHABET[(ALPHABET.index(c) - self.position) % 26]
